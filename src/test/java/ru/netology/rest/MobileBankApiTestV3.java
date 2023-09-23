06d8c2bdc465d5cbb7caa334d8652eb748e3994b
Denis Lysov <dan.lysov2015@yandex.ru>
2023-09-23 14:11:08 +0300

    Correct JSON Schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -22,7 +22,7 @@ class MobileBankApiTestV3 {
               // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
               .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
           .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUR"))
+          .body("[0].currency", equalTo("RUB"))
           .body("[0].balance", greaterThanOrEqualTo(0));
     }
 }
